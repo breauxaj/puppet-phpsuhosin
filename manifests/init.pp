53bde8325b45dbc3aef81a5f7414677fbb1b6674
Adam Breaux <breauxa@email.chop.edu>
2015-06-24 11:36:57 -0400

    dep hell

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,18 +3,8 @@ class phpsuhosin {
     /(?i-mx:centos|fedora|redhat|scientific)/ => [ 'php-suhosin' ],
   }
 
-  $depends = $::operatingsystem ? {
-    /(?i-mx:centos|fedora|redhat|scientific)/ => [ 'php' ],
-  }
-
-  $service = $::operatingsystem ? {
-    /(?i-mx:centos|fedora|redhat|scientific)/ => [ 'httpd' ],
-  }
-
   package { $required:
     ensure  => latest,
-    require => Package[$depends],
-    notify  => Service[$service],
   }
 
 }
